57bae22367e83d4f48b66077a9c1062fd562dd4f
Satyajit Ghana <satyajitghana7@gmail.com>
2019-03-30 21:09:17 +0530

    Add Solar System

--- app/src/main/java/com/cynergy/emulata/ARModelView.java
+++ app/src/main/java/com/cynergy/emulata/ARModelView.java
@@ -1,7 +1,10 @@
 package com.cynergy.emulata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.cynergy.emulata.armodels.solarsystem.SolarActivity;
 
 public class ARModelView extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class ARModelView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armodel_view);
+
+        Intent intent = new Intent(ARModelView.this, SolarActivity.class);
+        startActivity(intent);
     }
 }
